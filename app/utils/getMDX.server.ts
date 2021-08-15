1f6c579850ca9fdc0fbaef42b13ff36700908047
Girish21 <girishkumarv21@gmail.com>
2021-08-15 12:51:16 +0530

    remove containing div

--- app/utils/getMDX.server.ts
+++ app/utils/getMDX.server.ts
@@ -1,7 +1,29 @@
 import { bundleMDX } from "mdx-bundler";
 import fs from "fs";
 import path from "path";
+import type * as M from "mdast";
+import type * as H from "hast";
+
 import { remarkSyntaxHightlight } from "./remarkSyntaxHighlight.server";
+
+// adapted from https://github.com/kentcdodds/remix-kentcdodds/blob/main/app/utils/compile-mdx.server.ts#L141
+const rehypeReovePreContainingDiv = () => {
+  return async (tree: M.Root) => {
+    const { visit } = await import("unist-util-visit");
+
+    visit(
+      tree,
+      { type: "element", tagName: "pre" },
+      (node: H.Element, index, parent) => {
+        if (parent?.type !== "element") return;
+        const parentEl = parent as H.Element;
+        if (parentEl.tagName !== "div") return;
+        if (parentEl.children.length !== 1 && index === 0) return;
+        Object.assign(parentEl, node);
+      }
+    );
+  };
+};
 
 const getMDX = async (location: string) => {
   const file = fs.readFileSync(
@@ -23,6 +45,10 @@ const getMDX = async (location: string) => {
         remarkSlug,
         [remarkAutoLinkHeading, { behavior: "wrap" }],
         remarkSyntaxHightlight,
+      ];
+      options.rehypePlugins = [
+        ...(options.rehypePlugins ?? []),
+        rehypeReovePreContainingDiv,
       ];
 
       return options;
